93b2587d1274b2cbbfd3f23cc08bd72c7fc27c85
Vignesh G <107738847+Sweety-Vigneshg@users.noreply.github.com>
2024-03-02 17:56:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,197 @@
+// Function to navigate to department page
+function navigateToDepartment(department) {
+    if (department === 'CSE') {
+        window.location.href = "CSE.html";
+    } 
+    if (department === 'ECE') {
+        window.location.href = "ECE.html";
+    }
+    if (department === 'EEE') {
+         window.location.href = "EEE.html";
+    }
+    if (department === 'MECH') {
+        window.location.href = "MECH.html";
+    }
+    if (department === 'AIDS') {
+        window.location.href = "AIDS.html";
+    }
+    if (department === 'AIML') {
+        window.location.href = "AIML.html";
+    }
+    if (department === 'CIVIL') {
+        window.location.href = "CIVIL.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
+
+// Function to navigate to department page
+
+function navigateToYear(department) {
+    if (department === 'First_Year') {
+        window.location.href = "./CSE/First_Year.html";
+    } 
+    if (department === 'Second_Year') {
+        window.location.href = "./CSE/Second_Year.html";
+    }
+    if (department === 'Third_Year') {
+         window.location.href = "./CSE/Third_Year.html";
+    }
+    if (department === 'Fourth_Year') {
+        window.location.href = "./CSE/Fourth_Year.html";
+    }
+    if (department === 'PG_First_Year') {
+        window.location.href = "./CSE/PG_First_Year.html";
+    }
+    if (department === 'PG_Second_Year') {
+        window.location.href = "./CSE/PG_Second_Year.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
+
+function navigateToECEYear(department) {
+    if (department === 'First_Year') {
+        window.location.href = "./ECE/First_Year.html";
+    } 
+    if (department === 'Second_Year') {
+        window.location.href = "./ECE/Second_Year.html";
+    }
+    if (department === 'Third_Year') {
+         window.location.href = "./ECE/Third_Year.html";
+    }
+    if (department === 'Fourth_Year') {
+        window.location.href = "./ECE/Fourth_Year.html";
+    }
+    if (department === 'PG_First_Year') {
+        window.location.href = "./ECE/PG_First_Year.html";
+    }
+    if (department === 'PG_Second_Year') {
+        window.location.href = "./ECE/PG_Second_Year.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
+
+function navigateToEEEYear(department) {
+    if (department === 'First_Year') {
+        window.location.href = "./EEE/First_Year.html";
+    } 
+    if (department === 'Second_Year') {
+        window.location.href = "./EEE/Second_Year.html";
+    }
+    if (department === 'Third_Year') {
+         window.location.href = "./EEE/Third_Year.html";
+    }
+    if (department === 'Fourth_Year') {
+        window.location.href = "./EEE/Fourth_Year.html";
+    }
+    if (department === 'PG_First_Year') {
+        window.location.href = "./EEE/PG_First_Year.html";
+    }
+    if (department === 'PG_Second_Year') {
+        window.location.href = "./EEE/PG_Second_Year.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
+
+function navigateToMECHYear(department) {
+    if (department === 'First_Year') {
+        window.location.href = "./MECH/First_Year.html";
+    } 
+    if (department === 'Second_Year') {
+        window.location.href = "./MECH/Second_Year.html";
+    }
+    if (department === 'Third_Year') {
+         window.location.href = "./MECH/Third_Year.html";
+    }
+    if (department === 'Fourth_Year') {
+        window.location.href = "./MECH/Fourth_Year.html";
+    }
+    if (department === 'PG_First_Year') {
+        window.location.href = "./MECH/PG_First_Year.html";
+    }
+    if (department === 'PG_Second_Year') {
+        window.location.href = "./MECH/PG_Second_Year.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
+
+function navigateToCIVLYear(department) {
+    if (department === 'First_Year') {
+        window.location.href = "./CIVIL/First_Year.html";
+    } 
+    if (department === 'Second_Year') {
+        window.location.href = "./CIVIL/Second_Year.html";
+    }
+    if (department === 'Third_Year') {
+         window.location.href = "./CIVIL/Third_Year.html";
+    }
+    if (department === 'Fourth_Year') {
+        window.location.href = "./CIVIL/Fourth_Year.html";
+    }
+    if (department === 'PG_First_Year') {
+        window.location.href = "./CIVIL/PG_First_Year.html";
+    }
+    if (department === 'PG_Second_Year') {
+        window.location.href = "./CIVIL/PG_Second_Year.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
+
+function navigateToAIDSYear(department) {
+    if (department === 'First_Year') {
+        window.location.href = "./AIDS/First_Year.html";
+    } 
+    if (department === 'Second_Year') {
+        window.location.href = "./AIDS/Second_Year.html";
+    }
+    if (department === 'Third_Year') {
+         window.location.href = "./AIDS/Third_Year.html";
+    }
+    if (department === 'Fourth_Year') {
+        window.location.href = "./AIDS/Fourth_Year.html";
+    }
+    if (department === 'PG_First_Year') {
+        window.location.href = "./AIDS/PG_First_Year.html";
+    }
+    if (department === 'PG_Second_Year') {
+        window.location.href = "./AIDS/PG_Second_Year.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
+
+function navigateToAIMLYear(department) {
+    if (department === 'First_Year') {
+        window.location.href = "./AIML/First_Year.html";
+    } 
+    if (department === 'Second_Year') {
+        window.location.href = "./AIML/Second_Year.html";
+    }
+    if (department === 'Third_Year') {
+         window.location.href = "./AIML/Third_Year.html";
+    }
+    if (department === 'Fourth_Year') {
+        window.location.href = "./AIML/Fourth_Year.html";
+    }
+    if (department === 'PG_First_Year') {
+        window.location.href = "./AIML/PG_First_Year.html";
+    }
+    if (department === 'PG_Second_Year') {
+        window.location.href = "./AIML/PG_Second_Year.html";
+    }
+    else {
+       // window.location.href = department + "_AIML.html";
+    }
+}
